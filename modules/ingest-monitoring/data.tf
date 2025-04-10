b06db9f64ab1253534ba7ce67d2fa9ce105a569c
Wade Moore <wade@observeinc.com>
2025-04-10 12:52:40 -0400

    Update to use Tokens and Datastream resources from usage app

--- modules/ingest-monitoring/data.tf
+++ modules/ingest-monitoring/data.tf
@@ -7,3 +7,13 @@ data "observe_dataset" "license_events" {
   workspace = var.workspace.oid
   name      = "usage/License Ingest Events"
 }
+
+data "observe_dataset" "datastreams" {
+  workspace = var.workspace.oid
+  name      = "usage/Observe Datastream"
+}
+
+data "observe_dataset" "tokens" {
+  workspace = var.workspace.oid
+  name      = "usage/Observe Token"
+}
